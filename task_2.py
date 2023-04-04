52f3418aaf93ed25d6193666a2dff00fb842c6ca
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-04-04 07:05:02 +0300

    modified the jobs collector code

--- task_2.py
+++ task_2.py
@@ -3,75 +3,85 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-def job_scraper(url) -> list[dict]:
-    site_data = requests.get(url)
+def scrape_job_links(url):
+    try:
+        site_data = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return []
+
     soup = bs(site_data.text, "html.parser")
 
     job_boxes = soup.find_all("div", class_="content flex-auto")
 
     jobs_list = []
 
-    # create a dictionary containing the job title and the job link
-    # append the dictionary to the jobs_list list
     for item in job_boxes:
-        job_title = item.find("a").text
-        job_link = item.find("a")["href"]
+        try:
+            job_title = item.find("a").text.strip()
+            job_link = item.find("a")["href"]
+        except (AttributeError, KeyError) as e:
+            print(f"Error extracting job title/link: {e}")
+            continue
+
         job = {"job_title": job_title, "job_link": job_link}
         jobs_list.append(job)
 
     return jobs_list
 
 
-def inspect_jobs(jobs_list: list[dict]):
-    # go into the job link and inspect the job description
+def inspect_jobs(jobs_list):
     for job in jobs_list:
         job_link = job["job_link"]
-        job_data = requests.get(job_link)
-        job_soup = bs(job_data.text, "html.parser")
-        # the summary is has a h6 tag f class m-0
-        # the summary is in a div tag with class box-title border-bottom p-3
-        job_info_boxes = job_soup.find_all("div", class_="box-body p-3")
-        job_description = job_info_boxes[0].text
-        job_summary = job_info_boxes[1].text
+        try:
+            job_data = requests.get(job_link)
+        except requests.exceptions.RequestException as e:
+            print(f"Error retrieving job data: {e}")
+            continue
 
-        # add the job summary and job description to the job dictionary
+        job_soup = bs(job_data.text, "html.parser")
+        job_info_boxes = job_soup.find_all("div", class_="box-body p-3")
+
+        if len(job_info_boxes) < 2:
+            print("Error extracting job summary/description")
+            continue
+
+        job_summary = job_info_boxes[1].text.strip()
+        job_description = job_info_boxes[0].text.strip()
+
         job["job_summary"] = job_summary
         job["job_description"] = job_description
 
     return jobs_list
 
 
-def check_for_keywords(jobs_list: list[dict], keywords: list[str]):
-    # check if the job summary or job description contains the keywords
-    # if it does, add the job to a new list
-    # return the new list
-    new_list = []
-    for job in jobs_list:
-        job_summary = job["job_summary"]
-        job_description = job["job_description"]
-        for keyword in keywords:
-            if keyword in job_summary or keyword in job_description:
-                new_list.append(job)
-                break
-
-    # turn the list of dictionaries into a pandas dataframe
-    # save the dataframe as a csv file
-    # only include the job title and the job link
-    jobs_df = pd.DataFrame(new_list)
+def save_jobs_to_csv(jobs_list, filename):
+    jobs_df = pd.DataFrame(
+        jobs_list, columns=["job_title", "job_link", "job_summary", "job_description"]
+    )
     jobs_df = jobs_df[["job_title", "job_link"]]
-    jobs_df.to_csv("jobs.csv", index=False)
+    jobs_df.to_csv(filename, index=False)
+
+
+def check_for_keywords(jobs_list, keywords):
+    new_list = []
+    keywords_set = set(keywords)
+    for job in jobs_list:
+        job_summary = job["job_summary"].lower()
+        job_description = job["job_description"].lower()
+        if any(
+            keyword in job_summary or keyword in job_description
+            for keyword in keywords_set
+        ):
+            new_list.append(job)
+
+    return new_list
 
 
 if __name__ == "__main__":
-    scraped_data = job_scraper("https://www.myjobsinkenya.com/")
+    url = "https://www.myjobsinkenya.com/"
+    scraped_data = scrape_job_links(url)
     job_info = inspect_jobs(scraped_data)
-    keywords = [
-        "python",
-        "django",
-        "flask",
-        "data science",
-        "data analyst",
-        "data science",
-        "software",
-    ]
+    keywords = {"python", "django", "flask", "data science", "data analyst", "software"}
     keywords_jobs = check_for_keywords(job_info, keywords)
+    save_jobs_to_csv(keywords_jobs, "jobs.csv")
